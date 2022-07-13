a412914330eb87b3a8e2005b55e5e6635f24115b
Robert K <roborobo9a@gmail.com>
2022-07-13 02:24:55 +0200

    useHash

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,11 @@ import { AppComponent } from './app.component';
 const routes: Routes = [ ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, {
+      useHash: true,
+    }),
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
