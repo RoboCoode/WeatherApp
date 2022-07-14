dd04a4439157c1d80e4ea61a0baae91e8370b884
Robert K <roborobo9a@gmail.com>
2022-07-14 19:20:37 +0200

    tailwind: searchpanel+,Barlow+ etc.

--- src/app/search-panel/search-panel.component.ts
+++ src/app/search-panel/search-panel.component.ts
@@ -15,18 +15,18 @@ export class SearchPanelComponent implements OnInit {
 
   list_cities = [
     { name: 'Bratislava', cord: ['48.1482', '17.1067'] },
-    { name: 'Humenne', cord: ['48.9371', '21.9163'] },
-    { name: 'Koromla', cord: ['48.7163', '22.2926'] },
-    { name: 'Kosice', cord: ['48.714', '21.2581'] },
+    { name: 'Humenné', cord: ['48.9371', '21.9163'] },
+    { name: 'Koromľa', cord: ['48.7163', '22.2926'] },
+    { name: 'Košice', cord: ['48.714', '21.2581'] },
     { name: 'Michalovce', cord: ['48.7543', '21.9195'] },
     { name: 'Sobrance', cord: ['48.7445', '22.1814'] },
   ];
 
   rollback_cities = [
     { name: 'Bratislava', cord: ['48.1482', '17.1067'] },
-    { name: 'Humenne', cord: ['48.9371', '21.9163'] },
-    { name: 'Koromla', cord: ['48.7163', '22.2926'] },
-    { name: 'Kosice', cord: ['48.714', '21.2581'] },
+    { name: 'Humenné', cord: ['48.9371', '21.9163'] },
+    { name: 'Koromľa', cord: ['48.7163', '22.2926'] },
+    { name: 'Košice', cord: ['48.714', '21.2581'] },
     { name: 'Michalovce', cord: ['48.7543', '21.9195'] },
     { name: 'Sobrance', cord: ['48.7445', '22.1814'] },
   ];
