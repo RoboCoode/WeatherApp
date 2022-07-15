6eea59dd313e96040124a9160272d2f806ce347e
Robert K <roborobo9a@gmail.com>
2022-07-15 15:40:24 +0200

    corrected searching - rollback to old ver

--- src/app/search-panel/search-panel.component.ts
+++ src/app/search-panel/search-panel.component.ts
@@ -21,6 +21,17 @@ export class SearchPanelComponent implements OnInit {
     { name: 'Sobrance', cord: ['48.7445', '22.1814'], temp: '' },
   ];
 
+  
+  backupCities  = [
+    { name: 'Bratislava', cord: ['48.1482', '17.1067'], temp: '' },
+    { name: 'Humenné', cord: ['48.9371', '21.9163'], temp: '' },
+    { name: 'Koromľa', cord: ['48.7163', '22.2926'], temp: '' },
+    { name: 'Košice', cord: ['48.714', '21.2581'], temp: '' },
+    { name: 'Michalovce', cord: ['48.7543', '21.9195'], temp: '' },
+    { name: 'Sobrance', cord: ['48.7445', '22.1814'], temp: '' },]
+
+
+
   constructor(
     public apiserv: ApiservService,
     public storage: GlobStorageService
@@ -37,13 +48,12 @@ export class SearchPanelComponent implements OnInit {
 
   searchingCity(subs: string) {
     subs = subs.toLowerCase();
-    let backupCities = this.listCities;
-
+   
     if (subs == '') {
-      this.listCities = backupCities;
-      return this.listCities;
+      this.listCities = this.backupCities;
+      return  this.listCities ;
     } else {
-      return (this.listCities = backupCities.filter((e) =>
+      return (this.listCities = this.backupCities.filter((e) =>
         e.name.toLowerCase().includes(subs)
       ));
     }
